10456939b6cd9ae92a977d8d97eed98a64c9f7fd
Alhvi Balcarcel <abalcarcel@gmail.com>
2022-04-05 22:49:24 -0600

    added cimemachine camera

--- Assets/_MyProject/Scripts/PlayerMovement.cs
+++ Assets/_MyProject/Scripts/PlayerMovement.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     private CharacterController charController;
+    private Animator anim;
     public float speed = 5f;
     public float rotationSpeed = 5f;
     private Quaternion rotationQuat;
     private float t = 0;
+    private float speedFactor = 1;
 
     private void Start() {
         charController = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update() {
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speedFactor = 2f;
+        } else {
+            speedFactor = 1f;
+        }
+
         float movx = Input.GetAxis("Horizontal");
         float movz = Input.GetAxis("Vertical");
 
@@ -21,7 +31,7 @@ public class PlayerMovement : MonoBehaviour {
 
         if (movementVector.magnitude > 0.01) {
             //transform.forward = movementVector;
-            charController.SimpleMove(movementVector * speed);
+            charController.SimpleMove(movementVector * speed * speedFactor);
 
             rotationQuat = Quaternion.LookRotation(movementVector);
             t = 0f;
@@ -29,5 +39,7 @@ public class PlayerMovement : MonoBehaviour {
 
         t += Time.deltaTime;
         transform.rotation = Quaternion.Slerp(transform.rotation, rotationQuat, speed * t);
+
+        anim.SetFloat("Speed", movementVector.magnitude * speedFactor);
     }
 }
